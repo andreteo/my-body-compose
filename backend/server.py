63e4e479136f1995078b95b46454bf2c2b392c8b
andreteo <andreteosy@gmail.com>
2024-04-26 14:38:30 +0800

    Implement get user and edit user profile in backend

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,9 @@
-import os, bcrypt, psycopg2, pickle
+import os, bcrypt, psycopg2, pickle, json
 from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
+from sqlalchemy.orm import declarative_base, sessionmaker
 from dotenv import load_dotenv
 from pathlib import Path
-from sqlalchemy.orm import declarative_base, sessionmaker
 from tables.table import User, Profile
 
 from flask import Flask, request, jsonify, make_response
@@ -20,6 +20,7 @@ load_dotenv(dotenv_path)
 # Flask App
 app = Flask(__name__)
 CORS(app)
+
 
 # JWT Authentication
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
@@ -51,6 +52,10 @@ except (psycopg2.Error, Exception) as e:
     print(f"Error while connecting to PostgreSQL: {e}")
 
 
+def obj_to_dict(obj):
+    return {c.name: getattr(obj, c.name) for c in obj.__table__.columns}
+
+
 # APIs
 @app.route("/")
 def home():
@@ -61,6 +66,7 @@ def home():
 def register():
     if request.is_json:
         data = request.json
+        print(data)
         hashed_password = bcrypt.hashpw(
             data["password"].encode("utf-8"), bcrypt.gensalt()
         )
@@ -72,20 +78,25 @@ def register():
             date_of_birth=data["date_of_birth"],
             height=data["height"],
             weight=data["weight"],
-            profile_photo=pickle.dumps(data["profile_photo"]),
             phone_number=data["phone_number"],
-        )
-
-        new_user = User(
-            username=data["username"],
-            password=hashed_password.decode(),
-            email=data["email"],
-            profile_id=new_profile.profile_id,
+            profile_photo=data["profile_photo"],
+            bio=data["bio"],
         )
 
         try:
-            session.add_all([new_profile, new_user])
+            session.add(new_profile)
             session.commit()
+
+            new_user = User(
+                username=data["username"],
+                password=hashed_password.decode(),
+                email=data["email"],
+                profile_id=new_profile.profile_id,
+            )
+
+            session.add(new_user)
+            session.commit()
+
             return make_response(
                 jsonify({"ok": True, "message": "User registered successfully"}), 200
             )
@@ -115,16 +126,85 @@ def login():
             if bcrypt.checkpw(
                 data["password"].encode("utf-8"), user.password.encode("utf-8")
             ):
+                user_profile = (
+                    session.query(Profile).filter_by(profile_id=user.profile_id).first()
+                )
+
                 access_token = create_access_token(identity=data["username"])
+
                 response_data = {
-                    "ok": True,
-                    "message": "Login successful",
                     "token": access_token,
+                    "user_profile": obj_to_dict(user_profile),
                 }
                 return make_response(jsonify(response_data), 200)
             else:
                 response_data = {"ok": False, "message": "Bad Username or Password"}
                 return make_response(jsonify(response_data), 401)
+        else:
+            response_data = {"ok": False, "message": "User not found"}
+            return make_response(jsonify(response_data), 404)
+    else:
+        response_data = {"ok": False, "error": "Request must contain JSON data"}
+        return make_response(jsonify(response_data), 400)
+
+
+@app.route("/user/profile", methods=["GET"])
+@jwt_required()
+def get_user_profile():
+    user_found = get_jwt_identity()
+
+    if user_found:
+        user = session.query(User).filter_by(username=user_found).first()
+        user_profile = (
+            session.query(Profile).filter_by(profile_id=user.profile_id).first()
+        )
+
+        if user_profile:
+            profile_dict = obj_to_dict(
+                user_profile
+            )  # Convert SQLAlchemy object to dictionary
+            response_data = {"ok": True, "user_profile": profile_dict}
+            return make_response(jsonify(response_data), 200)
+        else:
+            response_data = {"ok": False, "message": "User profile not found"}
+            return make_response(jsonify(response_data), 404)
+    else:
+        response_data = {"ok": False, "message": "User not found"}
+        return make_response(jsonify(response_data), 404)
+
+
+@app.route("/user/profile/edit", methods=["POST"])
+@jwt_required()
+def edit_user_profile():
+    if request.is_json:
+        user_found = get_jwt_identity()
+
+        if user_found:
+            data = request.json
+
+            user = session.query(User).filter_by(username=user_found).first()
+            user_profile = (
+                session.query(Profile).filter_by(profile_id=user.profile_id).first()
+            )
+
+            if user_profile:
+                changed = []
+                for k, v in data.items():
+                    if data[k] != getattr(user_profile, k):
+                        setattr(user_profile, k, v)
+                        changed.append(k)
+
+                session.commit()
+                response_data = {
+                    "ok": True,
+                    "message": "Profile updated",
+                    "user_profile": obj_to_dict(user_profile),
+                    "changed": changed,
+                }
+                return make_response(jsonify(response_data), 200)
+            else:
+                response_data = {"ok": False, "message": "User profile not found"}
+                return make_response(jsonify(response_data), 404)
         else:
             response_data = {"ok": False, "message": "User not found"}
             return make_response(jsonify(response_data), 404)
--- backend/tables/table.py
+++ backend/tables/table.py
@@ -1,4 +1,15 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text, Float, ForeignKey, LargeBinary, LargeBinary
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Boolean,
+    DateTime,
+    Text,
+    Float,
+    ForeignKey,
+    LargeBinary,
+    LargeBinary,
+)
 from sqlalchemy.orm import declarative_base, relationship, backref
 from datetime import datetime
 
@@ -6,19 +17,19 @@ Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     user_id = Column(Integer(), primary_key=True)
     username = Column(String(50), nullable=False, unique=True)
     password = Column(String(60), nullable=False, unique=True)
     email = Column(String(255), nullable=True, unique=True)
     date_joined = Column(DateTime(), default=datetime.now)
-    profile_id = Column(Integer(), ForeignKey('profiles.profile_id'))
-    profile = relationship('Profile', backref=backref('user', uselist=False))
+    profile_id = Column(Integer(), ForeignKey("profiles.profile_id"))
+    profile = relationship("Profile", backref=backref("user", uselist=False))
 
 
 class Profile(Base):
-    __tablename__ = 'profiles'
+    __tablename__ = "profiles"
 
     profile_id = Column(Integer(), primary_key=True)
     first_name = Column(String(50), nullable=False)
@@ -28,5 +39,7 @@ class Profile(Base):
     height = Column(Float(), nullable=False)
     weight = Column(Float(), nullable=False)
     phone_number = Column(String(50), nullable=False)
-    profile_photo = Column(LargeBinary, nullable=True)
-
+    profile_photo = Column(Text, nullable=True)
+    before_photo = Column(Text, nullable=True)
+    after_photo = Column(Text, nullable=True)
+    bio = Column(Text, nullable=True)
